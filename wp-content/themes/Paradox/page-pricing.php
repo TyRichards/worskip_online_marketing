8250c387ae462c1ac5386ecd2fc6112832e781cb
Ty Richards <me@tyrichards.com>
2015-05-16 21:35:48 -0500

    pricing page redesign

--- wp-content/themes/Paradox/page-pricing.php
+++ wp-content/themes/Paradox/page-pricing.php
@@ -11,58 +11,152 @@ Template Name: Pricing
         <div class="row">
             <div class="col-md-12">
                 <h1 class="text-center title">Get a smoother rehearsal and a tighter band</h1>
+                <p class="text-center lead"><a class="lightbox" href="https://www.youtube.com/watch?v=TCo-Gbeq_bU"><i class="fa fa-youtube-play"></i> How do team plans work?</a></p>
             </div>
         </div>
-        <div class="row">                        
-            <div class="col-md-4 plan-1">
+        <div class="row plan-row"> 
+            <div class="col-md-2 plan-desc">
+                <div class="panel panel-default text-right">
+                    <div class="panel-heading" style="background:transparent;">
+                        <h3>&nbsp;</h3>                        
+                    </div> 
+                    <div class="panel-body" style="background:transparent;"> 
+                        <h4 class="price">Monthly Price</h4>           
+                        <ul class="features">
+                            <li>Price Per Member</li>
+                            <li>Total Members</li>
+                            <li>Song Lessons</li>
+                            <li>Acoustic & Electric Parts</li>
+                            <li>Charts & Tabs</li>
+                            <li>Share Song Lessons with Team</li>
+                        </ul>
+                    </div> <!-- .panel-body -->
+                </div> <!-- .panel -->
+            </div> <!-- .col -->                               
+            <div class="col-md-2 plan-1">
                 <div class="panel panel-default text-center">
                     <div class="panel-heading">
-                        <h3>Individual</h3>                        
+                        <h3>Solo</h3>                        
                     </div> 
                     <div class="panel-body"> 
-                        <h4 class="price">$10<span>/ mo.</span></h4>           
+                        <h4 class="price">$12</h4>           
                         <ul class="features">
-                            <li>1 User</li>
-                            <li>Access to All Song Lessons</li>
-                            <li>All Acoustic & Electric Parts</li>
-                            <li>Charts & Tabs</li>
-                            <li><strike>Video Sharing</strike></li>
+                            <li class="text-primary">$12 / member</li>
+                            <li>1</li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li>&nbsp;</li>
                         </ul>
                         <div class="call-to-action">
-                            <a class="btn btn-primary btn-large btn-primary btn-block" href="#">Start Free Trial</a>
+                            <a class="btn btn-primary btn-large btn-primary btn-block" href="http://app.worshiponline.com/trial">Start Free Trial</a>
                         </div> <!-- call-to-action -->         
                     </div> <!-- .panel-body -->
                 </div> <!-- .panel -->
             </div> <!-- .col -->
-            <div class="col-md-4 plan plan-2">
+            <div class="col-md-2 plan plan-2">
                 <div class="panel panel-default text-center">
                     <div class="panel-heading">
-                        <h3>Team</h3>                        
+                        <h3>Small Team</h3>                        
                     </div> 
                     <div class="panel-body">
                         <!-- <h4 class="price">$149<span>/ mo.</span></h4>                            -->
-                        <h4 class="price text-muted"><span class="text-muted" style="font-size:inherit;vertical-align:inherit;">?&nbsp;</span><span>/ mo.</span></h4>                           
+                        <h4 class="price">$32</h4>           
                         <ul class="features">
-                            <li><input type="text" class="form-control" id="exampleInputName" placeholder="How many are on your team?"></li>
-                            <li>Access to All Song Lessons</li>
-                            <li>All Acoustic & Electric Parts</li>
-                            <li>Charts & Tabs</li>
-                            <li>Video Sharing</li>
+                            <li class="text-primary">$6.40 / member</li>
+                            <li>Up to 5</li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
                         </ul>
                         <div class="call-to-action">
-                            <a class="btn btn-primary btn-large btn-primary btn-block" href="#">Start Free Trial</a>
+                            <a class="btn btn-primary btn-large btn-primary btn-block" href="http://app.worshiponline.com/trial">Start Free Trial</a>
                         </div> <!-- call-to-action -->         
                     </div> <!-- .panel-body -->
                 </div> <!-- .panel -->
             </div> <!-- .col --> 
-            <div class="col-md-3 col-md-offset-1 quotes">
-                <p class="text-muted"><i>"It’s like hiring a music director for only pennies a month."</i><br>— WO Customer</p>
-                <br />
-                <p class="text-muted"><i>"I can’t explain the feeling when all of your musicians show up knowing the songs perfectly! You have to try it!"</i><br>— WO Customer</p>
-                <br />
-                <p class="text-muted"><i>“Me and the other guitar player are in perfect sync, I’m having more fun than ever, and the band is benefiting big time!"</i><br>— WO Customer</p>
-            </div>                        
+            <div class="col-md-2 plan-3">
+                <div class="panel panel-default text-center">
+                    <div class="panel-heading">
+                        <h3>Medium Team</h3>                        
+                    </div> 
+                    <div class="panel-body"> 
+                        <h4 class="price">$52</h4>           
+                        <ul class="features">
+                            <li class="text-primary">$5.20 / member</li>
+                            <li>Up to 10</li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                        </ul>
+                        <div class="call-to-action">
+                            <a class="btn btn-primary btn-large btn-primary btn-block" href="http://app.worshiponline.com/trial">Start Free Trial</a>
+                        </div> <!-- call-to-action -->         
+                    </div> <!-- .panel-body -->
+                </div> <!-- .panel -->
+            </div> <!-- .col -->    
+           <div class="col-md-2 plan-4">
+                <div class="panel panel-default text-center">
+                    <div class="panel-heading">
+                        <h3>Large Team</h3>                        
+                    </div> 
+                    <div class="panel-body"> 
+                        <h4 class="price">$82</h4>           
+                        <ul class="features">
+                            <li class="text-primary">$4.10 / member</li>
+                            <li>Up to 20</li>                            
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                        </ul>
+                        <div class="call-to-action">
+                            <a class="btn btn-primary btn-large btn-primary btn-block" href="http://app.worshiponline.com/trial">Start Free Trial</a>
+                        </div> <!-- call-to-action -->         
+                    </div> <!-- .panel-body -->
+                </div> <!-- .panel -->
+            </div> <!-- .col -->   
+           <div class="col-md-2 plan-5">
+                <div class="panel panel-default text-center">
+                    <div class="panel-heading">
+                        <h3>XLarge Team</h3>                        
+                    </div> 
+                    <div class="panel-body"> 
+                        <h4 class="price">$120</h4>           
+                        <ul class="features">
+                            <li class="text-primary">$3.43 / member</li>
+                            <li>Up to 35</li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                            <li><i class="fa fa-check"></i></li>
+                        </ul>
+                        <div class="call-to-action">
+                            <a class="btn btn-primary btn-large btn-primary btn-block" href="http://app.worshiponline.com/trial">Start Free Trial</a>
+                        </div> <!-- call-to-action -->         
+                    </div> <!-- .panel-body -->
+                </div> <!-- .panel -->
+            </div> <!-- .col -->   
         </div> <!-- .row -->
+        <div class="row custom-desc">
+            <div class="col-sm-10 col-sm-offset-2">
+                <h4 class="text-center"><i class="fa fa-info-circle"></i> If you need a larger custom plan, please <a href="/support">send us a message</a>.</h4>
+            </div>
+        </div>
+        <hr />
+        <div class="row">
+            <div class="col-sm-4 quotes">
+                <p><i>"It’s like hiring a music director for only pennies a month."</i><br>— WO Customer</p>
+            </div>
+            <div class="col-sm-4 quotes">
+                <p><i>"I can’t explain the feeling when all of your musicians show up knowing the songs perfectly! You have to try it!"</i><br>— WO Customer</p>
+            </div>
+            <div class="col-sm-4 quotes">
+                <p><i>“Me and the other guitar player are in perfect sync, I’m having more fun than ever, and the band is benefiting big time!"</i><br>— WO Customer</p>
+            </div>   
+        </div>
     </div>
 </section>
 
